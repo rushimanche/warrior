af7e238bbd4a53da7425aff143e25d65c272a991
UzairJ99 <uzair-jawaid@hotmail.com>
2021-06-23 23:25:58 -0400

    cleaned some code and refactored Login class component

--- client/components/Login.js
+++ client/components/Login.js
@@ -7,57 +7,70 @@ import { loginUser } from '../services/UserService'
 import { withRouter } from 'next/router'
 import { withIronSession } from "next-iron-session";
 
-class Login extends Component {
+const Login = (props) => {
 
-  state = {
-    user: {}
+  // initial state of user
+  let initialCredentials = {
+    username: '',
+    password: ''
   }
 
-  loginUser = (e) => {
-      loginUser(this.state.user)
-        .then(response => {
-          var username = response['username'];
+  // React Hooks for user state
+  const [user, setUser] = React.useState(initialCredentials);
 
-          if (username) {
-            this.props.router.push({
-              pathname: '/home',
-              as: '/home',
-              query: { username: username }
-            });
-          }
-      });
+  /**
+   * Login the user when the user clicks submit.
+   * @param {HTMLElement} e the element event which was interacted with
+   * @returns {void}
+   */
+  const handleLogin = (e) => {
+    // call service
+    loginUser(user)
+    .then(response => {
+      var username = response['username'];
+      // go to users home page if they exist in the db response
+      if (username) {
+        this.props.router.push({
+          pathname: '/home',
+          as: '/home',
+          query: { username: username }
+        });
+      }
+    });
   }
 
-
-  onChangeForm = (e) => {
-      let user = this.state.user
-      if (e.target.name === 'username') {
-          user.username = e.target.value;
-      } else if (e.target.name === 'password') {
-          user.password = e.target.value;
-      } 
-      this.setState({user})
+  /**
+   * update user state when the user changes any input field values.
+   * @param {HTMLElement} e the element event which was interacted with
+   * @returns {void}
+   */
+  const onChangeForm = (e) => {
+    // destructure user state object based on which input changed
+    if (e.target.name === 'username') {
+      user = {...user, username: e.target.value};
+    } else if (e.target.name === 'password') {
+      user = {...user, password: e.target.value};
+    }
+    // update user state
+    setUser(user);
   }
 
-  render() {
-    
-    return (
-      <div className="Login">
-        <div className="container mrgnbtm">
-          <div className="row">
-            <div className="col-md-12">
-                <LoginUser 
-                  user={this.state.user}
-                  onChangeForm={this.onChangeForm}
-                  loginUser={this.loginUser}
-                  >
-                </LoginUser>
-            </div>
+  return (
+    <div className="Login">
+      <div className="container mrgnbtm">
+        <div className="row">
+          <div className="col-md-12">
+            <LoginUser 
+              user={user}
+              onChangeForm={onChangeForm}
+              loginUser={handleLogin}
+              >
+            </LoginUser>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default withRouter(Login);
